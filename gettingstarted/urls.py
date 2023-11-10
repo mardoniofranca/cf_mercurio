b1cd883d5441bff94626314e3f5151d5715990d3
mardonio franca <mardfranca@gmail.com>
2023-11-10 09:09:25 -0300

    update login - 09/11/23

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,40 +1,19 @@
-"""
-URL configuration for gettingstarted project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
-
+from django.urls import path, include
+admin.autodiscover()
 
 import hello.views
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),        
     path('accounts/', include('django.contrib.auth.urls')),
-    
-    path("",            hello.views.index      ,name="home"),
-
-    path("",            hello.views.index      ,name="index"),
+    path('',hello.views.index,name="home"),
+    path('menu/',hello.views.menu,name="menu"),
     path('cronograma1/',hello.views.cronograma1,name='cronograma1'),
     path('cronograma2/',hello.views.cronograma2,name='cronograma2'),
-    path('materiais/',  hello.views.materiais,  name='materiais'),
-    path('relatorios/', hello.views.relatorios, name='relatorios'),
-    path('dashboard/',  hello.views.dashboard,  name='dashboard'),
-    
-    
+    path('materiais/',hello.views.materiais,name='materiais'),
+    path('relatorios/',hello.views.relatorios,name='relatorios'),
+    path('dashboard/',hello.views.dashboard,name='dashboard'),
    
 ]
+
--- hello/views.py
+++ hello/views.py
@@ -1,30 +1,37 @@
-# blog/views.py
-from django.views.generic.list import ListView
+
 from django.shortcuts import render
 
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
 
-
-from .models import Post
-
-class BlogListView(ListView):
-    model = Post
-    template_name = 'inicio.html'
 
 
 def index(request):
      return render(request, 'inicio.html')
 
+@login_required(login_url='/accounts/login/')
+def menu(request):
+     return render(request, 'menu.html')
+
+@login_required(login_url='/accounts/login/')
 def cronograma1(request):
      return render(request, 'cronograma1.html')
 
+@login_required(login_url='/accounts/login/')
 def cronograma2(request):
      return render(request, 'cronograma2.html')
 
+@login_required(login_url='/accounts/login/')
 def materiais(request):
      return render(request, 'materiais.html')
 
+@login_required(login_url='/accounts/login/')
 def relatorios(request):
      return render(request, 'relatorios.html')
 
+@login_required(login_url='/accounts/login/')
 def dashboard(request):
      return render(request, 'dashboard.html')
